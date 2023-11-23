e2ecb098407c6bbe4912d6c22b7a2ddd562b1bc9
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-11-23 21:39:10 +0100

    putting stuff into derivations

--- flake.nix
+++ flake.nix
@@ -1,26 +1,89 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/23.05";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/23.05"; };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowBroken = true;
+      };
+    in {
+
+      packages.${system} = rec {
+        nixsearch_db = pkgs.stdenv.mkDerivation {
+            name = "nixsearch_db";
+            version = nixpkgs.rev;
+            src = ./.;
+            buildInputs = [
+                nixsearch_create_db
+            ];
+            buildPhase = ''
+                mkdir -p $out
+                cd $out
+                nixsearch_create_db
+            '';
+            installPhase = ''
+                echo pass
+            '';  
+        };
+                 
+        ns =     
+          pkgs.writers.writePython3Bin "ns" {
+            libraries = [ pkgs.python3Packages.sqlite-utils ];
+          } ''   
+          import os
+          import sqlite3
+
+          dbpath = "${nixsearch_create_db}/nix-search.db"
+          connection = sqlite3.connect(dbpath)
+          cursor = connection.cursor()
+          package = f"%{os.argv[1]}%"
+          rows = cursor.execute("SELECT * FROM packages WHERE name LIKE ? or description LIKE ? or longDescription LIKE ?", (package, package, package,)).fetchall()
+          for row in rows:
+              print("Package: {}\nVersion: {}\nDescription: {}\nLong Description:\n{}\n".format(*row))
+          '';
+        pkgs_json =
+          builtins.toFile "pkgs.json" (import ./main.nix { inherit pkgs; });
+        nixsearch_create_db =
+          pkgs.writers.writePython3Bin "nixsearch_create_db" {
+            libraries = [ pkgs.python3Packages.sqlite-utils ];
+          } ''
+            import json
+            import sqlite3
+
+
+            def get_tuple(name, x):
+                version = str(x["version"]) if "version" in x else ""
+                desc = x["description"] if "description" in x else ""
+                long_desc = x["longDescription"] if "longDescription" in x else ""
+                return (name, version, desc, long_desc)
+
+
+            con = sqlite3.connect("nix-search.db")
+            cur = con.cursor()
+            cur.execute("CREATE TABLE packages(name,\
+            version,\
+            description,\
+            longDescription)")
+            pkgs_json = "${pkgs_json}"
+            with open(pkgs_json, "r") as pkgs:
+                data = json.loads(pkgs.read())
+                to_insert = [get_tuple(name, x) for (name, x) in data.items()]
+                cur.executemany("INSERT INTO packages VALUES(?, ?, ?, ?)", to_insert)
+                con.commit()
+          '';
+
+      };
 
       devShells.${system} = {
         default = pkgs.mkShell {
           packages = with pkgs; [
-          #nix-eval-jobs
-          (python3.withPackages(ps: with ps; [
-            sqlite-utils
-            rich
-          ]))
-          sqlite
+            #nix-eval-jobs
+            (python3.withPackages (ps: with ps; [ sqlite-utils rich ]))
+            sqlite
           ];
         };
       };
--- main.nix
+++ main.nix
@@ -1,34 +1,21 @@
+{ pkgs }:
 let
-    flake = (builtins.getFlake "github:Nixos/nixpkgs");
     unknown = x: set: if (builtins.isAttrs set) && (builtins.hasAttr x set) then builtins.getAttr x set else "unknown";
     getDescription = pkg: if builtins.hasAttr "meta" pkg then
                     if builtins.hasAttr "description" pkg.meta then pkg.meta.description else "" else "";
     getLongDescription = pkg: if builtins.hasAttr "meta" pkg then
                     if builtins.hasAttr "longDescription" pkg.meta then pkg.meta.longDescription else "" else "";
 
-    pkgs = flake.legacyPackages.x86_64-linux;
-    isBroken = pkg: (builtins.isAttrs pkg) && (((builtins.hasAttr "broken" pkg) && (pkg.broken)) || ((builtins.hasAttr "meta" pkg) && (builtins.hasAttr "broken" pkg.meta) && pkg.meta.broken));
+    # isBroken = pkg: (builtins.isAttrs pkg) && (((builtins.hasAttr "broken" pkg) && (pkg.broken)) || ((builtins.hasAttr "meta" pkg) && (builtins.hasAttr "broken" pkg.meta) && pkg.meta.broken));
 in
 
-builtins.toJSON (builtins.mapAttrs (name: pkg: let r = builtins.tryEval pkg; in if r.success && (builtins.isAttrs pkg) && !(isBroken pkg) then {
+builtins.toJSON (builtins.mapAttrs (name: pkg: let r = builtins.tryEval pkg; in if r.success && (builtins.isAttrs pkg) then {
         name = name;
         version = unknown "version" pkg;
         description = getDescription pkg;
         longDescription = getLongDescription pkg;
 } else {}) (
-    pkgs //
+    #pkgs //
     pkgs.python3Packages //
-    pkgs.rPackages //
+    #pkgs.rPackages //
     {}))
-
-# builtins.toJSON (builtins.attrNames pkgs)
-
-# builtins.toJSON (
-#     builtins.mapAttrs (name: pkg: {
-#             name = unknown "name" pkg;
-#             version = unknown "version" pkg;
-#             description = getDescription pkg;
-#             longDescription = getLongDescription pkg;
-#         }) flake.legacyPackages.x86_64-linux)
-# 
-
