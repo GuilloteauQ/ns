af77c11f91b1e5dae09a3b856b5c6c7ecb933f0b
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2023-11-24 10:08:35 +0100

    format main.nix

--- main.nix
+++ main.nix
@@ -1,21 +1,35 @@
 { pkgs }:
 let
-    unknown = x: set: if (builtins.isAttrs set) && (builtins.hasAttr x set) then builtins.getAttr x set else "unknown";
-    getDescription = pkg: if builtins.hasAttr "meta" pkg then
-                    if builtins.hasAttr "description" pkg.meta then pkg.meta.description else "" else "";
-    getLongDescription = pkg: if builtins.hasAttr "meta" pkg then
-                    if builtins.hasAttr "longDescription" pkg.meta then pkg.meta.longDescription else "" else "";
+  unknown = x: set:
+    if (builtins.isAttrs set) && (builtins.hasAttr x set) then
+      builtins.getAttr x set
+    else
+      "unknown";
+  getDescription = pkg:
+    if builtins.hasAttr "meta" pkg then
+      if builtins.hasAttr "description" pkg.meta then
+        pkg.meta.description
+      else
+        ""
+    else
+      "";
+  getLongDescription = pkg:
+    if builtins.hasAttr "meta" pkg then
+      if builtins.hasAttr "longDescription" pkg.meta then
+        pkg.meta.longDescription
+      else
+        ""
+    else
+      "";
 
-    # isBroken = pkg: (builtins.isAttrs pkg) && (((builtins.hasAttr "broken" pkg) && (pkg.broken)) || ((builtins.hasAttr "meta" pkg) && (builtins.hasAttr "broken" pkg.meta) && pkg.meta.broken));
-in
+  # isBroken = pkg: (builtins.isAttrs pkg) && (((builtins.hasAttr "broken" pkg) && (pkg.broken)) || ((builtins.hasAttr "meta" pkg) && (builtins.hasAttr "broken" pkg.meta) && pkg.meta.broken));
 
-builtins.toJSON (builtins.mapAttrs (name: pkg: let r = builtins.tryEval pkg; in if r.success && (builtins.isAttrs pkg) then {
-        name = name;
-        version = unknown "version" pkg;
-        description = getDescription pkg;
-        longDescription = getLongDescription pkg;
-} else {}) (
-    #pkgs //
-    pkgs.python3Packages //
-    pkgs.rPackages //
-    {}))
+in builtins.toJSON (builtins.mapAttrs (name: pkg:
+  let r = builtins.tryEval pkg;
+  in if r.success && (builtins.isAttrs pkg) then {
+    name = name;
+    version = unknown "version" pkg;
+    description = getDescription pkg;
+    longDescription = getLongDescription pkg;
+  } else
+    { }) (pkgs // pkgs.python3Packages // pkgs.rPackages // { }))
